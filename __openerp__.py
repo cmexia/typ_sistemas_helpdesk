107eb76c4369525580e65590962c6f76e62d894e
Carlos Mexia <carlos.lia.hmo@gmail.com>
2015-11-06 08:47:17 -0700

    Load de archivos

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'TyP sistemas helpdesk',
+    'version': '0.1',
+    "author" : "Cmexia",
+    "category" : "TyP",
+    'description': """
+    Este modulo permite llevar un manejo extendido de las incidencias segun como lo requiere la compañia
+
+    """,
+    "website" : "www.typrefrigeracion.com.mx",
+    "license" : "AGPL-3",
+    "depends" : ['project','project_issue'],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data": [ 
+        ],
+    "update_xml" : [
+    
+        "project_view.xml",
+                    ],
+    "installable" : True,
+    "active" : False,
+}
---
+++ project.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+
+from openerp.osv import fields, osv, orm
+
+class project_issue(osv.osv):
+    _name = 'project.issue'
+    _inherit = 'project.issue'
+    _columns = {
+        # 'category_id':fields.many2one('project.issue.category','Categoria'),
+        'issue_category':fields.selection(
+            (('1','Velocidad'),('2','Incidencia'),('3','Desarrollo'),
+             ('4','Capacitacion'),('5','Proceso'),('6','Control de acceso'),
+             ),'Categoria'),
+        'company_id':fields.many2one('res.company','Sucursal', required=True),
+        'priority':fields.selection(
+            (('1','Urgente'),
+             ('2','Media'),('3','Baja'),
+             ),'Prioridad', help="URGENTE: Detiene operación o causa retrabajos MEDIA: todas BAJA: mejoras, cambios en diseño, formato, campos adicionales, etc."),
+        'deadline':fields.date('Fecha compromiso'),
+        'notes':fields.text('Incidencia'),
+        'solution':fields.text('Solucion'),
+    }
+
+# class project_issue_category(osv.osv):
+#     _name = 'project.issue.category'
+#     _description = 'Category of Issues'
+#     _columns = {
+#         'issue_category': fields.char('categoria', size=60),
+#         'category_id':fields.one2many('project_issue','category_id','Categoria')
+#     }
+
+class company_assignment(osv.osv):
+    _name = 'res.company'
+    _inherit = 'res.company'
+    _columns = {
+        'company_id': fields.one2many('project.issue','company_id','Sucursal'),
+
+    }
+    
